9caa64aaa618bea779fd938b434642c33b021d9f
Aleksei Ostriakov <alexey.ostryakov@rydlab.ru>
2024-01-24 10:26:42 +0500

    [FIX] l10n_ru: update module version

--- __manifest__.py
+++ __manifest__.py
@@ -15,7 +15,7 @@
     "author": "RYDLAB",
     "website": "https://rydlab.ru",
     "category": "Localization",
-    "version": "1.0",
+    "version": "14.0.1.0.0",
     "depends": ["base", "account", "uom", "product"],
     "pre_init_hook": "pre_init_hook",
     "data": [
